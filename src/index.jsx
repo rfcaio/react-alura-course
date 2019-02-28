bb9a7ba908978a51236c8841c29c9db6009ad06a
Caio Rodrigues <rfcaio90@gmail.com>
2019-02-28 12:43:29 -0300

    create `Author` component

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Author from './Author'
+
+const authors = [
+  { id: 1, name: 'Michael Moorcock', email: 'michael.moorcock@mail.com' },
+  { id: 2, name: 'Robert E. Howard', email: 'robert.e.howard@mail.com' },
+  { id: 3, name: 'H. P. Lovecraft', email: 'h.p.lovecraft@mail.com' }
+]
+
 const App = () => (
-  <h1>A simple React app.</h1>
+  <main>
+    <h1>A simple React app.</h1>
+
+    <section>
+      <Author authors={authors} />
+    </section>
+  </main>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
