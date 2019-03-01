6a71fa1eb192d88d764104476bad5a04810ea9b5
Caio Rodrigues <rfcaio90@gmail.com>
2019-03-01 11:11:59 -0300

    define `Author` as a stateful component

--- src/Author/index.jsx
+++ src/Author/index.jsx
@@ -3,11 +3,21 @@ import React from 'react'
 import AuthorForm from './AuthorForm'
 import AuthorTable from './AuthorTable'
 
-const Author = ({ authors }) => (
-  <div>
-    <AuthorForm />
-    <AuthorTable authors={authors} />
-  </div>
-)
+class Author extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { authors: props.authors }
+  }
+
+  render () {
+    let { authors } = this.state
+    return (
+      <div>
+        <AuthorForm />
+        <AuthorTable authors={authors} />
+      </div>
+    )
+  }
+}
 
 export default Author
