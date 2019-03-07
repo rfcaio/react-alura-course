721bd21affa367f2ce81190e8f21c4a07b593d11
Caio Rodrigues <rfcaio90@gmail.com>
2019-03-07 12:09:21 -0300

    create route definitions

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 
 import Author from './Author'
 import Home from './Home'
 
-const App = () => (
+const App = props => (
   <main>
-    <Home />
-    <section>
-      <Author />
-    </section>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/authors">Authors</Link>
+        </li>
+      </ul>
+    </nav>
+    <section>{props.children}</section>
   </main>
 )
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render((
+  <Router>
+    <App>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/authors" component={Author} />
+      </Switch>
+    </App>
+  </Router>
+), document.getElementById('root'))
